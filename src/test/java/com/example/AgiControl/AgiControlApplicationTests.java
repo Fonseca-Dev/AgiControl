d51e32ffd80133b90755012e85dc4943b9cb2d83
okauefonseca <124893260+Fonseca-Dev@users.noreply.github.com>
2025-10-21 15:26:50 -0300

    Add files via upload

---
+++ src/test/java/com/example/AgiControl/AgiControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AgiControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
